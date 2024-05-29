281e348df8ef705688c20c07ecc1e9aa30e9681e
Chayapol Wongpuwarak <kongkcypw@gmail.com>
2024-05-29 16:26:15 +0700

    fix: cors preflight

--- config/cookie-config.js
+++ config/cookie-config.js
@@ -1,15 +1,13 @@
-const isProduction = process.env.NODE_ENV === 'production';
-
 const cookieConfig = {
     options: {
         httpOnly: true,
-        secure: isProduction,
+        secure: false,
         maxAge: 60 * 60 * 1000, // 1 hour
         Credentials: true,
     },
     refreshOptions: {
         httpOnly: false,
-        secure: isProduction,
+        secure: false,
         maxAge: 24 * 60 * 60 * 1000, // 24 hour
         Credentials: true,
     },
