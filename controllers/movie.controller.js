696da0df9f5b6fd74266fb151bd6f173559e94e2
Chayapol Wongpuwarak <kongkcypw@gmail.com>
2024-05-29 12:43:20 +0700

    add: movie controller for home page

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,4 +1,4 @@
-const { insertMovie, checkRepeatedMovieId, getAllMovie } = require("../models/movie.model");
+const { insertMovie, checkRepeatedMovieId, getAllMovie, updateMovie, deleteMovie, getPublicMoviesByYearRange } = require("../models/movie.model");
 const { uuidv7 } = require('uuidv7');
 const { getAllRateId } = require("../models/rate.model");
 
@@ -12,20 +12,53 @@ const generateUniqueMovieId = async () => {
     return movieId;
 };
 
+exports.get_public = async (req, res) => {
+    try {
+        const { startYear, endYear } = req.body;
+        const movie = await getPublicMoviesByYearRange(startYear, endYear);
+        res.status(200).json({ message: "get all movie success", movie: movie });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: 'An error occurred while get movie list',
+        });
+    }
+}
+
+
+exports.get_all = async (req, res) => {
+    try {
+        const permissionLevel = req.user.permissionLevel;
+        console.log(`permissionLevel ${permissionLevel}`);
+
+        if (permissionLevel < 1) {
+            return res.status(423).json({ message: "you don't have permission to get all field of movieId" });
+        }
+        const movie = await getAllMovie();
+        const rate = await getAllRateId();
+
+        res.status(200).json({ message: "get all movie success", movie: movie, rate: rate });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: 'An error occurred while get movie list',
+        });
+    }
+};
 
 exports.add_new = async (req, res) => {
     try {
         const permissionLevel = req.user.permissionLevel;
         console.log(`permissionLevel ${permissionLevel}`);
-        
-        if(permissionLevel < 1) {
-            return res.status(423).json({ message: "you don't have permission to add movie"});
+
+        if (permissionLevel < 1) {
+            return res.status(423).json({ message: "you don't have permission to add movie" });
         }
 
         const { movieTitle, yearReleased, rating, movieImageUrl } = req.body;
 
-        if(!movieTitle || !yearReleased || !rating || !movieImageUrl){
-            return res.status(400).json({ message: "all input is require"});
+        if (!movieTitle || !yearReleased || !rating || !movieImageUrl) {
+            return res.status(400).json({ message: "all input is require" });
         }
 
         const movieId = await generateUniqueMovieId();
@@ -40,22 +73,42 @@ exports.add_new = async (req, res) => {
     }
 };
 
-exports.get_all = async (req, res) => {
+exports.update = async (req, res) => {
     try {
+        const { movieId, movieTitle, rating, movieImageUrl, yearReleased } = req.body;
         const permissionLevel = req.user.permissionLevel;
         console.log(`permissionLevel ${permissionLevel}`);
-        
-        if(permissionLevel < 1) {
-            return res.status(423).json({ message: "you don't have permission to get all field of movieId"});
-        }
-        const movie = await getAllMovie();
-        const rate = await getAllRateId();
 
-        res.status(200).json({ message: "get all movie success", movie: movie, rate: rate });
+        if (permissionLevel < 1) {
+            return res.status(423).json({ message: "you don't have permission to update movie" });
+        }
+        const result = await updateMovie(movieId, movieTitle, yearReleased, rating, movieImageUrl);
+
+        res.status(200).json({ message: "update movie success", result: result });
     } catch (error) {
         console.error(error);
         res.status(500).json({
-            message: 'An error occurred while add new movie',
+            message: 'An error occurred while update movie',
+        });
+    }
+};
+
+exports.delete = async (req, res) => {
+    try {
+        const { movieId } = req.body;
+        const permissionLevel = req.user.permissionLevel;
+        console.log(`permissionLevel ${permissionLevel}`);
+
+        if (permissionLevel < 1) {
+            return res.status(423).json({ message: "you don't have permission to delete movie" });
+        }
+        const result = await deleteMovie(movieId);
+
+        res.status(200).json({ message: "delete movie success", result: result });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: 'An error occurred while update movie',
         });
     }
 };
